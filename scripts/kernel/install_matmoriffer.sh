9dfa0a7ac6dd9582cccbdcd842315ee09b5ae9b0
Swistusmen <michal76aw@gmail.com>
2024-01-10 21:01:20 +0100

    scripts-also-install

--- scripts/kernel/install_matmoriffer.sh
+++ scripts/kernel/install_matmoriffer.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 sudo make -C ../../kernel/
-if [[ ! -d "/lib/modules/$(uname -r)/kernel/drivers/matmoriffer" ]] then
+if [ ! -d "/lib/modules/$(uname -r)/kernel/drivers/matmoriffer" ]; then
     sudo mkdir "/lib/modules/$(uname -r)/kernel/drivers/matmoriffer"
 fi 
 
-sudo cp ../../kernel/source/matmoriffer.ko /lib/modules/$(uname -r)/kernel/drivers/matmoriffer/matmoriffer.ko
+module_in_destination=/lib/modules/$(uname -r)/kernel/drivers/matmoriffer/matmoriffer.ko
+if [ ! -e $module_in_destination ]; then
+    sudo cp ../../kernel/source/matmoriffer.ko $module_in_destination
+else
+    echo "Matmoriffer is present, updating"
+    sudo rmmod matmoriffer
+    sudo rm $module_in_destination
+    sudo cp ../../kernel/source/matmoriffer.ko $module_in_destination
+fi
 
-if [[ ! -d "/etc/modules-load.d/matmorrifer.conf" ]] then
+if [ ! -d "/etc/modules-load.d/matmorrifer.conf" ]; then
     echo "matmoriffer" >> "/etc/modules-load.d/matmorrifer.conf"
 fi 
 
 sudo depmod -a
-sudo insmod ../../kernel/source/matmoriffer.ko
+sudo insmod $module_in_destination
--- scripts/netshield/netshield_install.sh
+++ scripts/netshield/netshield_install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 destination="/usr/local/netshield"
-if [[ -d $destination ]] then
-    return
+if [ -d $destination ]; then
+    echo "NetShield is already installed- updating"
+    sudo rm $destination/NetShield
+    sudo cp $(pwd)/../NetShield $destination/NetShield
 else
     sudo mkdir $destination
     sudo cp $(pwd)/../NetShield $destination/NetShield
@@ -11,14 +13,14 @@ else
 fi
 
 application_shortcut=$HOME/Desktop
-if [[ -d "$HOME/Desktop" ]] then
+if [ -d "$HOME/Desktop" ]; then
     sudo mkdir $HOME/.local/share/applications/netshield
     echo "[Desktop Entry]" > $HOME/.local/share/applications/netshield/NetShield.desktop
     echo "Name=NetShield" >> $HOME/.local/share/applications/netshield/NetShield.desktop
     echo "Exec=/usr/local/netshield/NetShield" >> $HOME/.local/share/applications/netshield/NetShield.desktop
     echo "Type=Application" >> $HOME/.local/share/applications/netshield/NetShield.desktop
     sudo update-desktop-database
-    if [[ ! -e "$HOME/Desktop/NetShield.desktop "]] then
+    if [ ! -e "$HOME/Desktop/NetShield.desktop "]; then
         sudo cp $HOME/.local/share/applications/netshield/NetShield.desktop $HOME/Desktop/NetShield.desktop
         gio set $HOME/Desktop/NetShield.desktop "metadata::trusted" true
     fi
