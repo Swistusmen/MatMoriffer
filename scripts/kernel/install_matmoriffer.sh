b1fc06e8f6a7861d2886cfa9bf15b98b6bfd0d39
Swistusmen <michal76aw@gmail.com>
2024-01-10 21:17:26 +0100

    uninstalling

--- scripts/kernel/install_matmoriffer.sh
+++ scripts/kernel/install_matmoriffer.sh
@@ -7,12 +7,12 @@ fi
 
 module_in_destination=/lib/modules/$(uname -r)/kernel/drivers/matmoriffer/matmoriffer.ko
 if [ ! -e $module_in_destination ]; then
-    sudo cp ../../kernel/source/matmoriffer.ko $module_in_destination
+    sudo cp ../../kernel/src/matmoriffer.ko $module_in_destination
 else
     echo "Matmoriffer is present, updating"
     sudo rmmod matmoriffer
     sudo rm $module_in_destination
-    sudo cp ../../kernel/source/matmoriffer.ko $module_in_destination
+    sudo cp ../../kernel/src/matmoriffer.ko $module_in_destination
 fi
 
 if [ ! -d "/etc/modules-load.d/matmorrifer.conf" ]; then
---
+++ scripts/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo ./kernel/uninstall.sh
+sudo ./netshield/uninstall_netshield.sh
